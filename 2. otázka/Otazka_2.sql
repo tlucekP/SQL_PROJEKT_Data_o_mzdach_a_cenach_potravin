d2f2379d2a331999451f51107396cc6ec11f823e
Peter Tlučhoř <tluchor.peter@outlook.cz>
2024-06-01 16:48:46 +0200

    SQL rework
    WITH - mleko update

--- 2. otázka/Otazka_2.sql
+++ 2. otázka/Otazka_2.sql
@@ -18,55 +18,11 @@ WHERE price IS NOT NULL
 ORDER BY `year` DESC;
 
 
-SELECT
-	a.`year`,
-	a.job_category,
-	a.milk_per_wage,
-	b.bread_per_wage
-FROM
-	(SELECT
-	`year`,
-	job_category,
-	round(avg(wage/price), 0) AS milk_per_wage
+WITH MilkData AS (
+	SELECT
+		`year`,
+		ROUND(avg(wage/price), 0) AS milk_per_wage
 	FROM t_peter_tluchor_project_sql_primary_final tptpspf
 	WHERE food LIKE '%mleko%'
 		AND price IS NOT NULL
-		AND `year` = 2006
-	GROUP BY job_category) a
-LEFT JOIN
-	(SELECT
-	job_category,
-	round(avg(wage/price), 0) AS bread_per_wage
-	FROM t_peter_tluchor_project_sql_primary_final tptpspf
-	WHERE food LIKE '%chleb%'
-		AND price IS NOT NULL
-		AND `year` = 2006
-	GROUP BY job_category) b
-ON a.job_category = b.job_category;
-
-
-SELECT
-	a.`year`,
-	a.job_category,
-	a.milk_per_wage,
-	b.bread_per_wage
-FROM
-	(SELECT
-	`year`,
-	job_category,
-	round(avg(wage/price), 0) AS milk_per_wage
-	FROM t_peter_tluchor_project_sql_primary_final tptpspf
-	WHERE food LIKE '%mleko%'
-		AND price IS NOT NULL
-		AND `year` = 2018
-	GROUP BY job_category) a
-LEFT JOIN
-	(SELECT
-	job_category,
-	round(avg(wage/price), 0) AS bread_per_wage
-	FROM t_peter_tluchor_project_sql_primary_final tptpspf
-	WHERE food LIKE '%chleb%'
-		AND price IS NOT NULL
-		AND `year` = 2018
-	GROUP BY job_category) b
-ON a.job_category = b.job_category;
+	GROUP BY `year`
